f9493b2c0ff2436364a44e00c2ecc9a6acad437a
Haneen Haashlamoun <haneen.hashlamoun@gmail.com>
2021-09-14 00:15:25 +0300

    finish lab12 add atlas db and create function

--- server.js
+++ server.js
@@ -7,19 +7,25 @@ const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connect(`${MONGO_URL}`);
 
-const { getBooks } = require('./controllers/book.controllers.js');
+const {
+    getBooks,
+    createBook,
+    deleteBook } = require('./controllers/book.controllers.js');
 
 const getTest = require('./controllers/test.controller.js');
-
 const seedBook = require('./helper/bookSeed.seed.js');
 // seedBook();
 
 app.get('/test', getTest);
 app.get('/books', getBooks);
+app.post('/books', createBook);
+app.delete('/books/:book_id', deleteBook);
+
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
